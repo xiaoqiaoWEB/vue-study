1cb68704bdb705b800efd6013761ae79f31c635a
xiaoqiaoWEB <346043591@qq.com>
2020-03-08 17:08:02 +0800

    21

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
+    <Form></Form>
   </div>
 </template>
+
+<script>
+import Form from 'components/form/index.vue'
+export default {
+  name: 'App',
+  components: {
+    Form
+  }
+}
+</script>
 
 <style lang="less">
 </style>
